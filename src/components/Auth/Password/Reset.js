2d981a8541082a58228e5c106d2945b384330dd2
nemanja-z <nemanja14zivanovic@gmail.com>
2021-01-28 19:21:15 +0100

    Implement password reset

--- src/components/Auth/Password/Reset.js
+++ src/components/Auth/Password/Reset.js
@@ -1,25 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import {useParams, useHistory} from "react-router-dom";
+import api from '../../../services/api';
+import cookie from '../../../services/cookie';
 
+const fields = {
+		email:"",
+		password:"",
+		password_confirmation:""
+	};
 const Reset = () =>{
+	const [data, setData] = useState(fields);
+	const {token} = useParams();
+	const history = useHistory();
+	const formData = new FormData();
+	formData.append("email", data.email);
+	formData.append("password", data.password);
+	formData.append("password_confirmation", data.password_confirmation);
+	formData.append("token", token);
 
+	const handleReset = async(e)=>{
+		try{
+			e.preventDefault();
+			await cookie();
+			const {data:{success}} = await api().post('api/reset-password', formData);
+			if(success){
+				history.push('/login');
+				setData(fields);
+			}
+		}catch(e){
+			console.log(e);
+		}
+	}
 
     return(
 			<div className="md:flex md:justify-center mt-6 mb-6">
 			
-				<form className="flex flex-col items-center bg-gray-200 shadow-md rounded">
+				<form className="flex flex-col items-center bg-gray-200 shadow-md rounded" onSubmit={handleReset}>
 					<div >
 						<label htmlFor="email" className="block mb-2">E-Mail Address</label>
-							<input id="email" type="email" name="email" required autofocus />
+							<input id="email" type="email" name="email" required onChange={e=>setData({...data, email:e.target.value})} autoFocus />
 					</div>
 
 					<div >
 						<label htmlFor="password" className="block mb-2">Password</label>
-						<input id="password" type="password" name="password"  required />
+						<input id="password" type="password" name="password" onChange={e=>setData({...data, password:e.target.value})} required />
 					</div>
 
 					<div >
 						<label htmlFor="password-confirm" className="block mb-2">Confirm Password</label>
-							<input id="password-confirm" type="password" name="password_confirmation" required />
+							<input id="password-confirm" type="password" name="password_confirmation" onChange={e=>setData({...data, password_confirmation:e.target.value})} required />
 					</div>
 					<div >
 							<button type="submit" className="font-bold text-sm text-blue-500 hover:text-blue-800">
